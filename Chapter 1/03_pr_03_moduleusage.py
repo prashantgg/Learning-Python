b9c35d1e2940bb1f2592be9a9c5badcaebbf1d3e
Prashant Sapkota <prashantsapkota016@gmail.com>
2024-05-07 14:28:52 +0545

    Day 1 Chapter 1

---
+++ Chapter 1/03_pr_03_moduleusage.py
@@ -0,0 +1,2 @@
+from playsound import playsound  # type: ignore
+playsound('C:\\Users\\prash\\OneDrive\\Desktop\\Learn Python Harry\\Chapter 1\\play.mp3')
---
+++ Chapter 1/04_pr_04_listdir.py
@@ -0,0 +1,5 @@
+# Author : Prashant Sapkota
+# Location : Kapan
+# Date : 5/1/2024
+import os #importing os
+print (os.listdir()) #printing list of direcotory 
